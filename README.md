8b1cdffe59bc915c230697b7760bd75b0e5e5745
Mr-MichaelK <michael1kolanjian2@gmail.com>
2024-07-19 18:01:58 +0300

    Add the playGround() function
    I also added the documentation that explains how this function works and
    added some comments to explain certain functionalities in the previous
    functions.

--- README.md
+++ README.md
@@ -13,7 +13,13 @@ For the time being, the game will have no GUI, so it will be played in the conso
 
 ##### getComputerChoice():
 Generates a random integer from 0 to 2.
+
 0 returns rock, 1 returns paper, 2 returns scissors
 
 ##### getHumanChoice():
 Takes a string input from the user which is strictly in the set {"rock", "paper", "scissors"} (case insensitive) and returns it.
+
+##### playRound():
+Takes the player and computer choices respectively and declares the winner accordingly.
+
+The winner gets their score incremented. In case of a draw, neither player nor computer gets any points.
--- root/script.js
+++ root/script.js
@@ -1,6 +1,7 @@
 function getComputerChoice() {
     let max = 2;
     let min = 0;
+    // generate an int in the range [0,2]
     let choice = Math.floor(Math.random() * (max - min + 1)) + min;
     if (choice === 0)      return "rock";
     else if (choice === 1) return "paper";
@@ -9,10 +10,31 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let choice = prompt("Rock, Paper, Scissors GO!").toLowerCase();
+    // ensure input is valid
     while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
         choice = prompt("Invalid choice! Rock, Paper, Scissors GO!");
     }
     return choice;
 }
 
-console.log(getHumanChoice());
+let humanScore    = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    // draw scenario
+    if (humanChoice === computerChoice)
+        return "Draw!";
+
+    // winning scenarios
+    if (humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "paper" && computerChoice === "rock"
+        || humanChoice === "scissors" && computerChoice === "paper") 
+    {
+        humanScore++;
+        return "Player wins this round!";
+    }
+    
+    // losing scenario remains
+    computerScore++;
+    return "Computer wins this round!";
+}
